01c022c17993b2035b6864b7a3679e296fb76153
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-02-16 02:38:25 +0530

    updated

--- CH-FE/src/components/socialScreens/Home.jsx
+++ CH-FE/src/components/socialScreens/Home.jsx
@@ -3,21 +3,19 @@ import NavbarAfterLogin from './common/NavbarAfterLogin'
 import Leftbar from './common/Leftbar';
 import Rightbar from './common/Rightbar'
 import Feedbar from './middlebars/Feedbar';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import {Container,Row, Col} from 'react-bootstrap';
 
 function Home() {
   return <>
     <NavbarAfterLogin/>
 
-    {/* <Container fluid>
+    <Container fluid>
       <Row>
         <Col ><Leftbar/></Col>
         <Col xs lg={8}><Feedbar/></Col>
         <Col><Rightbar/></Col>
       </Row>
-    </Container> */}
+    </Container>
   </>
 }
 
