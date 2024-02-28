1bbf12c73ff670e6bab2659e818cca3aec2ad353
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-02-28 17:02:59 +0530

    updated

--- CH-FE/src/components/socialScreens/middlebars/Feedbar.jsx
+++ CH-FE/src/components/socialScreens/middlebars/Feedbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Row, Col,Button,Card} from 'react-bootstrap';
+import {Row, Col,Button,Card,Form} from 'react-bootstrap';
 
 function Feedbar() {
   const feedCards = [
@@ -9,30 +9,43 @@ function Feedbar() {
     // "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fb%2Fb6%2FImage_created_with_a_mobile_phone.png%2F1200px-Image_created_with_a_mobile_phone.png&tbnid=peRqpDgIPrDI-M&vet=12ahUKEwiLtOTP0fCDAxU6waACHeISAJwQMygAegQIARBL..i&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FImage&docid=0JWe7yDOKrVFAM&w=1200&h=900&q=image&ved=2ahUKEwiLtOTP0fCDAxU6waACHeISAJwQMygAegQIARBL"
   ]
   return <>
-    <div className="feedArea mt-3">
-      {
-        feedCards.map((e,i)=>{
-          return <div key={i}>
-            <Col>
-              <Card className='mb-5 postFeed mx-auto' style={{ width: '95%' }}>
-                <Card.Img variant="top" src={e}/>
-                <Card.Text className='ms-2'>0 Likes</Card.Text>
-                <div className='d-flex flex-row'>
+    <div className='mt-3'>
+      <Form>
+        <div>
+          <Form.Group className="feedInputArea">
+            <Form.Control type='text' placeholder='Write Something here...'/>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control type='text' placeholder='Write Something here...'/>
+          </Form.Group>
+        </div>
+        <Button variant="primary" type="submit">Create Feed</Button>
+      </Form>
+      <div className="feedArea mt-3">
+        {
+          feedCards.map((e,i)=>{
+            return <div key={i}>
+              <Col>
+                <Card className='mb-5 postFeed mx-auto' style={{ width: '95%' }}>
+                  <Card.Img variant="top" src={e}/>
                   <Card.Text className='ms-2'>0 Likes</Card.Text>
-                  <Card.Text className='ms-3'>0 Comments</Card.Text>
-                </div>
-                <Card.Body className='p-0'>
-                  <Row>
-                    <Col style={{paddingRight:"0px"}}><Button variant="light" className='likeBtn' style={{ width: '100%' }}>Like</Button></Col>
-                    <Col style={{padding:"0px"}}><Button variant="light" className='commentBtn' style={{ width: '100%' }}>Comment</Button></Col>
-                    <Col style={{paddingLeft:"0px"}}><Button variant="light" className='reportBtn' style={{ width: '100%' }}>Report</Button></Col>
-                  </Row>
-                </Card.Body>
-              </Card>
-            </Col>
-          </div>
-        })
-      }
+                  <div className='d-flex flex-row'>
+                    <Card.Text className='ms-2'>0 Likes</Card.Text>
+                    <Card.Text className='ms-3'>0 Comments</Card.Text>
+                  </div>
+                  <Card.Body className='p-0'>
+                    <Row>
+                      <Col style={{paddingRight:"0px"}}><Button variant="light" className='likeBtn' style={{ width: '100%' }}>Like</Button></Col>
+                      <Col style={{padding:"0px"}}><Button variant="light" className='commentBtn' style={{ width: '100%' }}>Comment</Button></Col>
+                      <Col style={{paddingLeft:"0px"}}><Button variant="light" className='reportBtn' style={{ width: '100%' }}>Report</Button></Col>
+                    </Row>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </div>
+          })
+        }
+      </div>
     </div>
   </>
 }
